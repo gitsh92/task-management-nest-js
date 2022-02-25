2278647a620ad68deb2ec8a49f77305be9edbd9f
Stuart Huggins <stuhuggins@gmail.com>
2022-02-25 18:05:49 +0000

    initial commit

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as path from 'path';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'taskmanagement',
+  entities: [path.join(__dirname, '..', '**/*.entity.{js,ts}')],
+  synchronize: true
+};
---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,46 @@
+import { User } from 'src/auth/user.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from './dtos/create-task.dto';
+import { GetTasksFilterDto } from './dtos/get-tasks-filter.dto';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
+
+@EntityRepository(Task)
+export class TaskRepository extends Repository<Task> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
+    const { status, search } = filterDto;
+
+    const query = this.createQueryBuilder('task');
+
+    query.where('task.userId = :userId', { userId: user.id });
+
+    if (status) {
+      query.andWhere('LOWER(task.status) = LOWER(:status)', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
+        { search: `%${search}%` }
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    task.user = user;
+    await task.save();
+
+    delete task.user;
+
+    return task;
+  }
+}
